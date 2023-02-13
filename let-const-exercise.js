ee1651397a0830181c7c2ab1ef255046711ce9a9
JoelACedeno <123585946+JoelACedeno@users.noreply.github.com>
2023-02-13 15:07:36 -0500

    Add files via upload

---
+++ let-const-exercise.js
@@ -0,0 +1,15 @@
+var PI = 3.14;
+PI = 42; // stop me from doing this!
+
+/* Write an ES2015 Version */
+
+const = 3.14;
+
+//Quiz
+//What is the difference between var and let?
+Var is for general variables, let is for variables meant to be changed
+//What is the difference between var and const?
+Var is for general variables, const is for variables not meant to be changed
+//What is the difference between let and const?
+let is for variables intended to be changed or reassigned and const is for variables not meant to be changed    
+//What is hoisting?
